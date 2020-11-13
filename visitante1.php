0f75e6f44b0d1293369e14c84b7312dea11bf94c
Professor <Professor@WIN-MP3JADQD58A.local>
2020-11-13 11:51:06 +0000

    Update formulario.html, principal.php, and 2 more files...

---
+++ visitante1.php
@@ -0,0 +1,20 @@
+<?php
+	echo "<span style='color:red;'><h3><b>**********************Pagina Visitante 1********************</b></h3></span><br>";
+
+	$nome2=$_COOKIE['nome'];
+	$idade2=$_COOKIE['idade'];
+
+	echo "Nome: ".$_COOKIE['nome']."<br>";
+	echo "Idade: ".$_COOKIE['idade']."<br>";
+
+	echo "<br><br>";
+	echo 'Nome: ' .$nome2. '<br>';
+	echo 'Idade: ' .$idade2;
+
+
+
+	echo '<a href="formulario.html"><h5><b>Voltar</b></h5></a>';
+	echo '<a href="principal.php"><h5><b>principal</b></h5></a>';
+	echo '<a href="visitante2.php"><h5><b>Visitante 2</b></h5></a>';
+
+?>
